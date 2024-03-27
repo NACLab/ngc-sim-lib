277ea4dd56536857ddcef4777b78014cc674603e
Alexander Ororbia <ago@hal3.cs.rit.edu>
2024-03-27 12:27:03 -0400

    updated doc compliance in controller

--- ngclib/controller.py
+++ ngclib/controller.py
@@ -4,14 +4,18 @@ import json, os, warnings, inspect
 
 class Controller:
     """
-    The ngc controller is the foundation of all ngclearn models. The controller is the object that organizes all the
-    components, commands, and connections for a model.
+    The ngc controller is the foundation of all ngclearn models and the central
+    operational construct for simulating complex systems. The controller is the
+    object that organizes all of the components, commands, and connections
+    that characterize a complex system and/or model to be simulated over time
+    (and was referred to as the "nodes-and-cables" system in earlier versions of
+    ngc-learn).
     """
     def __init__(self):
         self.steps = []
         self.commands = {}
-        self.components = {}
-        self.connections = []
+        self.components = {} ## components/nodes that characterize system/simulation object
+        self.connections = []  ## cables that characterize system/simulation object
 
         self._json_objects = {
             "commands": [],
@@ -25,17 +29,26 @@ class Controller:
 
     def runCycle(self, **kwargs):
         """
-        Runs all the commands that have been added to a cycle in the order they were added. All keyword arguments that
-        are needed to run the commands in the cycle are passed in here and will be forwarded to each command.
-        :param kwargs: all keyword arguments that are needed to run every command in the cycle
+        Runs all of the commands that have been added to a "compute" cycle in
+        the order that they were added. All of the keyword arguments that are
+        needed to run the commands within the cycle are passed in here and will
+        ultimately be forwarded to each command.
+
+        Args:
+            kwargs: all keyword arguments that are needed to run every command
+                within the cycle
         """
         for step in self.steps:
             self[step](**kwargs)
 
     def verify_connections(self, skip_components=None):
         """
-        Loops through all components in the controller and calls the `verify_connections` method on each.
-        :param skip_components: a list of component names to skip over while verifying connections (default None)
+        Loops through all components within the controller and calls the
+        `verify_connections` method on/for each.
+
+        Args:
+            skip_components: a list of component names to skip over while
+                verifying connections (Default: None)
         """
         for component in self.components.keys():
             if skip_components is not None and component.name in skip_components:
@@ -46,14 +59,22 @@ class Controller:
     def connect(self, source_component_name, source_compartment_name, destination_component_name,
                 target_compartment_name, bundle=None):
         """
-        Creates a cable from one component to another
+        Creates a cable from one component to another.
 
-        :param source_component_name: the name of the component providing the value
-        :param source_compartment_name: the name of the compartment containing the source value
-        :param destination_component_name: the name of the component receiving the value
-        :param target_compartment_name: the name of the compartment to store the source value in
-        :param bundle: the number of the bundle rule to be used when using this cable (default None)
-        :return:
+        Args:
+            source_component_name: the name of the component providing the value
+
+            source_compartment_name: the name of the compartment containing the
+                source value
+
+            destination_component_name: the name of the component receiving
+                the value
+
+            target_compartment_name: the name of the compartment to store the
+                source value in
+
+            bundle: the number of the bundle rule to be used when using this
+                cable (Default: None)
         """
         self.components[destination_component_name].create_incoming_connection(
             self.components[source_component_name].create_outgoing_connection(source_compartment_name), target_compartment_name,
@@ -68,8 +89,12 @@ class Controller:
 
     def make_connections(self, path_to_cables_file):
         """
-        Loads a collection of cables from a json file. Follow `cables.schema` for the format of the json file.
-        :param path_to_cables_file: the path to the file, including the name and extension
+        Loads a collection of cables from a json file. Follow `cables.schema`
+        for the specific format of the required json file.
+
+        Args:
+            path_to_cables_file: the path to the file, including the name and
+                extension
         """
         with open(path_to_cables_file, 'r') as file:
             cables = json.load(file)
@@ -78,10 +103,16 @@ class Controller:
 
     def make_components(self, path_to_components_file, custom_file_dir=None):
         """
-        Loads a collection of components from a json file. Follow `components.schema` for the format of the json file.
-        :param path_to_components_file: the path to the file, including the name and extension
-        :param custom_file_dir: the path to the custom directory for custom load methods, this directory is named
-        `custom` if the save_to_json method is used. (default None)
+        Loads a collection of components from a json file. Follow
+        `components.schema` for the specific format of the json file.
+
+        Args:
+            path_to_components_file: the path to the file, including the name
+                and extension
+
+            custom_file_dir: the path to the custom directory for custom load methods,
+                this directory is named `custom` if the save_to_json method is
+                used. (Default: None)
         """
         with open(path_to_components_file, 'r') as file:
             components = json.load(file)
@@ -90,8 +121,11 @@ class Controller:
 
     def make_steps(self, path_to_steps_file):
         """
-        Loads a collection of steps from a json file. Follow `steps.schema` for the format of this json file
-        :param path_to_steps_file: the path of the file, including the name and extension
+        Loads a collection of steps from a json file. Follow `steps.schema` for
+        the specific format of this json file.
+
+        Args:
+            path_to_steps_file: the path of the file, including the name and extension
         """
         with open(path_to_steps_file, 'r') as file:
             steps = json.load(file)
@@ -100,8 +134,12 @@ class Controller:
 
     def make_commands(self, path_to_commands_file):
         """
-        Loads a collection of commands from a json file. Follow `commands.schema` for the format of this json file
-        :param path_to_commands_file: the path of the file, including the name and extension
+        Loads a collection of commands from a json file. Follow `commands.schema`
+        for the specific format of this json file.
+
+        Args:
+            path_to_commands_file: the path of the file, including the name
+                and extension
         """
         with open(path_to_commands_file, 'r') as file:
             commands = json.load(file)
@@ -110,9 +148,12 @@ class Controller:
 
     def add_step(self, command_name):
         """
-        Adds a command to the built-in cycle. When the cycle is run the commands will be called in the order they are
-        added in.
-        :param command_name: The name of the command to be added
+        Adds a command to the built-in cycle. When the cycle is run/executed,
+        the commands will be called in the order that they are added in (when
+        setting up the controller simulation object).
+
+        Args:
+            command_name: the name of the command to be added
         """
         if command_name not in self.commands.keys():
             raise RuntimeError(str(command_name) + " is not a registered command")
@@ -122,18 +163,29 @@ class Controller:
     def add_component(self, component_type, match_case=False, absolute_path=False, **kwargs):
         """
         Acts as a component factory for the controller.
-        :param component_type: A string that is linked to the component class to be created. If the class was loaded
-        with the modules.json file this can be the keywords defined in that file. Otherwise, it will have to be
-        dynamically loaded using the functions found in ngclib.utils.
-        :param match_case: A boolean that represents if the exact case should be matched when dynamically loading the
-        component class (default False)
-        :param absolute_path: A boolean that represents if the component class should be treated as an absolute path
-        when dynamically loading the component class (default False)
-        :param kwargs: All the keyword arguments that are needed to initialize the loaded component class. The function
-        will try to crash nicely if keyword arguments are missing. This list of arguments will also be stored to allow
-        for the component to be rebuilt, but if a given value is not serializable it will drop that from the keyword
-        arguments.
-        :return: The created component (Component is also automatically added to the controller)
+
+        Args:
+            component_type: A string that is linked to the component class to be
+                created. If the class was loaded with the modules.json file this
+                can be the keywords defined in that file. Otherwise, it will have
+                to be dynamically loaded using the functions found in ngclib.utils.
+
+            match_case: A boolean that represents if the exact case should be
+                matched when dynamically loading the component class (Default: False)
+
+            absolute_path: A boolean that represents if the component class
+                should be treated as an absolute path when dynamically loading
+                the component class (Default: False)
+
+            kwargs: All of the keyword arguments that are needed to initialize
+                the loaded component class. The function will try to crash nicely
+                if keyword arguments are missing. This list of arguments will
+                also be stored to allow for the component to be rebuilt, but if
+                a given value is not serializable it will drop that from the
+                keyword arguments.
+
+        Returns:
+            the created component (Component is also automatically added to the controller)
         """
         Component_class = load_from_path(path=component_type, match_case=match_case, absolute_path=absolute_path)
 
@@ -172,27 +224,43 @@ class Controller:
     def add_command(self, command_type, command_name, match_case=False, absolute_path=False, component_names=None,
                     **kwargs):
         """
-        Acts as a factory to create command.
-        In addition to adding command objects to the controllers command list, commands are also set to their attributes
-        on the controller. For example if a command named `step` is added myController.runCommand("step", ...) is equivalent
-        to myController.step(...). There is
-        :param command_type: A string that is linked to the command class to be created. If the class was loaded
-        with the modules.json file this can be the keywords defined in that file. Otherwise, it will have to be
-        dynamically loaded using the functions found in ngclib.utils.
-        :param command_name: A string that is the name of the command, this is the keyword that will be called
-        elsewhere to execute this command.
-        :param match_case: A boolean that represents if the exact case should be matched when dynamically loading the
-        command class (default False)
-        :param absolute_path: A boolean that represents if the command class should be treated as an absolute path
-        when dynamically loading the command class (default False)
-        :param component_names: A list of component names to be passed to the command's constructor. Internally it will
-        convert the strings to the actual component objects so they must exist in the controller prior to this function
-        being called.
-        :param kwargs: All the keyword arguments that are needed to initialize the loaded command class. The function
-        will try to crash nicely if keyword arguments are missing. This list of arguments will also be stored to allow
-        for the component to be rebuilt, but if a given value is not serializable it will drop that from the keyword
-        arguments.
-        :return: The created command (Command is also automatically added to the controller)
+        Acts as a factory to create/synthesize commands.
+        In addition to adding command objects to the controllers internal
+        command list, commands are also set to their attributes on the controller.
+        For example, if a command named `step` is added myController.runCommand("step", ...)
+        is equivalent to myController.step(...).
+
+        Args:
+            command_type: A string that is linked to the command class to be
+                created. If the class was loaded with the modules.json file this
+                can be the keywords defined in that file. Otherwise, it will
+                have to be dynamically loaded using the functions found
+                in ngclib.utils.
+
+            command_name: A string that is the name of the command, this is the
+                keyword that will be called elsewhere to execute this command.
+
+            match_case: A boolean that represents if the exact case should be
+                matched when dynamically loading the command class (Default: False)
+
+            absolute_path: A boolean that represents if the command class should
+                be treated as an absolute path when dynamically loading the
+                command class (Default: False)
+
+            component_names: A list of component names to be passed to the
+                command's constructor. Internally it will convert the strings to
+                the actual component objects so they must exist in the controller
+                prior to this function being called.
+
+            kwargs: All the keyword arguments that are needed to initialize the
+                loaded command class. The function will try to crash nicely if
+                keyword arguments are missing. This list of arguments will also
+                be stored to allow for the component to be rebuilt, but if a
+                given value is not serializable it will drop that from the
+                keyword arguments.
+
+        Returns:
+            the created command (Command is also automatically added to the controller)
         """
         Command_class = load_from_path(path=command_type, match_case=match_case, absolute_path=absolute_path)
         if not callable(Command_class):
@@ -236,10 +304,14 @@ class Controller:
 
     def runCommand(self, command_name, *args, **kwargs):
         """
-        Runs the given command
-        :param command_name: The name of the command to run
-        :param args: positional arguments to be passed into the command
-        :param kwargs: keyword arguments to be passed into the command
+        Runs the given command.
+
+        Args:
+            command_name: the name of the command to run
+
+            args: positional arguments to be passed into the command
+
+            kwargs: keyword arguments to be passed into the command
         """
         command = self.commands.get(command_name, None)
         if command is None:
@@ -250,13 +322,21 @@ class Controller:
         """
         Dumps all the required json files to rebuild the current controller to a specified directory. If there is a
         `save` command present on the controller and custom_save is True, it will run that command as well.
-        :param directory: The top level directory to save the model to
-        :param model_name: The name of the model, if None or if there is already a model with that name a uid will be
-        used or appended to the name respectively. (default None)
-        :param custom_save: A boolean that if true will attempt to call the `save` command if present on the controller
-        (default True)
-        :return: A tuple where the first value is the path to the model, and the second is the path to the custom save
-        folder if custom_save is true and None if false
+
+        Args:
+            directory: The top level directory to save the model to
+
+            model_name: The name of the model, if None or if there is already a
+                model with that name a uid will be used or appended to the name
+                respectively. (Default: None)
+
+            custom_save: A boolean that if true will attempt to call the `save`
+                command if present on the controller (Default: True)
+
+        Returns:
+            a tuple where the first value is the path to the model, and the
+                second is the path to the custom save folder if custom_save is
+                true and None if false
         """
         path = make_unique_path(directory, model_name)
 
@@ -283,9 +363,14 @@ class Controller:
 
     def load_from_dir(self, directory, custom_folder="/custom"):
         """
-        Builds a controller from a directory. Designed to be used with save_to_json
-        :param directory: The path to the model
-        :param custom_folder: The name of the custom data folder for building components. (default `/custom`)
+        Builds a controller from a directory. Designed to be used with
+        `save_to_json`.
+
+        Args:
+            directory: the path to the model
+
+            custom_folder: The name of the custom data folder for building
+                components. (Default: `/custom`)
         """
         self.make_components(directory + "/components.json", directory + custom_folder)
         self.make_connections(directory + "/connections.json")
