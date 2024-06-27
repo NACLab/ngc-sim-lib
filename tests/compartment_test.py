2d13b60d76539795aed26407705fd1d40b2697e2
Viet Nguyen <alexvn.edu@gmail.com>
2024-06-27 09:45:51 -0400

    config example unit testing

--- tests/compartment_test.py
+++ tests/compartment_test.py
@@ -1,21 +1,22 @@
 
 import pathlib
 import sys
-from functools import partial as bind
 
 # NOTE: VN: Since we have installed using `pip install -e .` in the workflow file, we
 # might not need to manually add to the path
 sys.path.append(str(pathlib.Path(__file__).parent.parent))
 
-import ngcsimlib
+# import ngcsimlib
 
-def test_import():
-  success = False
-  try:
-    from ngcsimlib.compartment import Compartment
-    success = True
-  except:
+class CompartmentTest:
+  
+  def test_import(self):
     success = False
-  assert success, "Import failed!"
+    try:
+      from ngcsimlib.compartment import Compartment
+      success = True
+    except:
+      success = False
+    assert success, "Import failed!"
 
 
