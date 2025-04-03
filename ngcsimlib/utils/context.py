79072ff1bcefa0d1c89f3fbd2a27dd2ff1d97290
Will Gebhardt <will@gebhardts.net>
2025-04-03 14:33:21 -0400

    updated context helpers
    Added functionality to get context from root, and inferContext if contexts are named differently from saved and current models

--- ngcsimlib/utils/context.py
+++ ngcsimlib/utils/context.py
@@ -19,10 +19,33 @@ def get_current_path():
 
 def get_context(path):
     """
-    A helper method for getting a context by a provided path
+    A helper method for getting a context by a provided path, to search from the
+    root path start the path with '/'
     """
+    if path[0] == "/":
+        return __contexts.get(path, None)
+
     return __contexts.get(__current_context + "/" + path, None)
 
+
+def infer_context(path, trailing_path=1):
+    """
+    A helper method that attempts to get the given context by a provided path, if
+    the context does not exist it will return the current context
+    Args:
+        path: path to where the context should be
+        trailing_path: how many trailing path locations should be ignored in
+        general 1 for components, 2 for compartments
+
+
+    Returns: located context object or the current context if unable to locate
+    given context
+    """
+    ctx = get_context("/".join(path.split("/")[:-trailing_path]))
+
+    if ctx is None:
+        return get_current_context()
+    return ctx
 
 def add_context(name, con):
     """
